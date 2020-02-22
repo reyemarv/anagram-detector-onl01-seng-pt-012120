adb97701325912766b8a571e88d90a7454b4c51d
Marvin Reyes <reyemarv@gmail.com>
2020-02-22 19:50:32 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
-# Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end 
+  
+  def match(anagrams)
+    anagram_list = []
+   anagrams.each do |items| 
+   if items.split("").sort == @word.split("").sort
+     anagram_list << items
+   end 
+ end
+ anagram_list
+ end 
+end 
