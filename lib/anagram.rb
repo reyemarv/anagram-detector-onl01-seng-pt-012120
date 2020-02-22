b440a0191c9b30562cf70342cb84394760056d81
Marvin Reyes <reyemarv@gmail.com>
2020-02-22 19:39:18 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -2,13 +2,12 @@ class Anagram
   
   attr_accessor :word
   
-  anagram_list = []
-  
   def initialize(word)
     @word = word
   end 
   
   def match(anagrams)
+    anagram_list = []
    anagrams.each {|items| items.split(" ") == @word.split(" ") ? 
     anagram_list << items : anagram_list}
     anagram_list
