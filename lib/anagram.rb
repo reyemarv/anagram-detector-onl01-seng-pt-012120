840a09bd338ad10caba2b05e774e2f063bd36b41
Marvin Reyes <reyemarv@gmail.com>
2020-02-22 19:28:19 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,11 +5,12 @@ class Anagram
   anagram_list = []
   
   def initialize(word)
-    @word = word.split(" ")
+    @word = word
   end 
   
   def match(anagrams)
-    some_words = %w(anagrams).each_with_index do {|i| i.split.sort
-    some_words
+    letters = @word.split(" ")
+    some_words = %w(anagrams).each_with_index do {|i| i.split.sort}
+    letters == some_words ? 
   end 
 end 
