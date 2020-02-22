291021d8181840e177240397121cd08fda0a5e19
Marvin Reyes <reyemarv@gmail.com>
2020-02-22 19:31:20 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -11,6 +11,6 @@ class Anagram
   def match(anagrams)
     letters = @word.split(" ")
     some_words = %w(anagrams).each_with_index do {|i| i.split.sort}
-    letters == some_words ? 
+     
   end 
 end 
